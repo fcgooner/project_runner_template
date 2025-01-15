5b5e2c0f337b03162a01629a44706ec22dd69b03
fcgooner <r.maniachuk@gmail.com>
2025-01-15 09:39:58 +0200

    Update launch_profile.py
    remove headless mode

--- draft/launch_profile.py
+++ draft/launch_profile.py
@@ -6,7 +6,8 @@ async def launch_profile():
     port = 00000
     profile_id = 'grt56jd'
 
-    open_url = f'http://local.adspower.net:{port}/api/v1/browser/start?user_id={profile_id}&open_tabs=1&ip_tab=0&launch_args=["--headless"]'
+    #open_url = f'http://local.adspower.net:{port}/api/v1/browser/start?user_id={profile_id}&open_tabs=1&ip_tab=0&launch_args=["--headless"]'
+    open_url = f'http://local.adspower.net:{port}/api/v1/browser/start?user_id={profile_id}&open_tabs=1'
     close_url = f"http://local.adspower.net:{port}/api/v1/browser/stop?user_id={profile_id}"
 
     # payload = {
